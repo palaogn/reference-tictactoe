9a0f04c1fc4dad9fb7243e2622df912207a0cc80
palaogn <palaogn@gmail.com>
2016-12-07 14:47:49 +0000

    added the git commit value as a tag for the docker image, fixed a bug in DOCKERFILE where I was using /build

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,10 @@
 FROM node
 MAINTAINER Pala Ogn <pala15@ru.is>
 WORKDIR /code
-COPY /build .
+COPY . .
 RUN npm install --silent
-RUN npm install -g create-react-app --silent
-RUN npm install -g nodemon --silent
 RUN ls
 EXPOSE 3000
 ENV NODE_PATH .
 CMD ["node", "run.js"]
-CMD ["./runMigratedb.sh"]
+CMD ["./scripts/runMigratedb.sh"]
---
+++ scripts/dockerBuild.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#if the docker image doesn't have a label, than this sets it's value to the current Head commit
+if [ -z "$GIT_COMMIT" ]; then
+  export GIT_COMMIT=$(git rev-parse HEAD)
+  export GIT_URL=$(git config --get remote.origin.url)
+fi
+
+# Remove .git from url in order to get https link to repo (assumes https url for GitHub)
+export GITHUB_URL=$(echo $GIT_URL | rev | cut -c 5- | rev)
+
+#Builds the app
+echo Building app
+npm run build
+
+#Checks if the build failed or not
+rc=$?
+if [[ $rc != 0 ]] ; then
+    echo "Npm build failed with exit code " $rc
+    exit $rc
+fi
+
+#Createsagithas.txt file that contains the latest git head
+cat > ./.env <<_EOF_
+GIT_COMMIT=$GIT_COMMIT
+_EOF_
+
+#Copying files to build folder
+cp ./Dockerfile ./build/
+cp ./package.json ./build/
+cp -r ./scripts ./build
+#cp ./bin/runMigratedb.sh ./build/
+#cp ./bin/cleanDocker.sh ./build/
+#cp ./bin/dockerBuild.sh ./build/
+
+
+#build docker image
+cd build
+echo Building docker image
+
+docker build -t palaogn/tictactoe:$GIT_COMMIT .
+
+#checks if the docker build failed or not
+rc=$?
+if [[ $rc != 0 ]] ; then
+    echo "Docker build failed " $rc
+    exit $rc
+fi
+
+#pushes the docker image to dockerhub, check if it failed or nor
+docker push palaogn/tictactoe:$GIT_COMMIT
+rc=$?
+if [[ $rc != 0 ]] ; then
+    echo "Docker push failed " $rc
+    exit $rc
+fi
+
+echo "Done"
