818d12bcc2aa6321b08d2d7bfc0a72a94f5e76ab
palaogn <palaogn@gmail.com>
2016-12-07 00:10:31 +0000

    added npm install -g create-react-app and npm install -g nodemon in Dockerfile

--- dockerScript.sh
+++ dockerScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+#if the docker image doesn't have a label, than this sets it's value to the current Head commit
 if [ -z "$GIT_COMMIT" ]; then
   export GIT_COMMIT=$(git rev-parse HEAD)
   export GIT_URL=$(git config --get remote.origin.url)
@@ -9,37 +9,40 @@ fi
 # Remove .git from url in order to get https link to repo (assumes https url for GitHub)
 export GITHUB_URL=$(echo $GIT_URL | rev | cut -c 5- | rev)
 
-
+#Builds the app
 echo Building app
-
 npm build
+npm run build
 
+#Checks if the build failed or not
 rc=$?
 if [[ $rc != 0 ]] ; then
     echo "Npm build failed with exit code " $rc
     exit $rc
 fi
 
-npm run build
-
 #Copying files to build folder
 cp ./Dockerfile ./build/
 cp ./package.json ./build/
 cp ./runMigratedb.sh ./build/
+cp ./cleanDocker.sh ./build/
+cp ./dockerScript.sh ./build/
 
 
-
+#build docker image
 cd build
 echo Building docker image
 
 docker build -t palaogn/tictactoe:$GIT_COMMIT .
 
+#checks if the docker build failed or not
 rc=$?
 if [[ $rc != 0 ]] ; then
     echo "Docker build failed " $rc
     exit $rc
 fi
 
+#pushes the docker image to dockerhub, check if it failed or nor
 docker push palaogn/tictactoe:$GIT_COMMIT
 rc=$?
 if [[ $rc != 0 ]] ; then
